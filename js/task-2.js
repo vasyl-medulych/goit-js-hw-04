53372e9f4e87c2460d3d2eabf5cf4cbd96c14ced
Vasyl Linkin <linkinmvv@gmail.com>
2025-06-21 14:45:59 +0300

    tasks complate

--- js/task-2.js
+++ js/task-2.js
@@ -1,18 +1,42 @@
 'use strict';
-console.log('Задача 2. Композиція масивів');
-function makeArray(firstArray, secondArray, maxLength) {
-  const array = firstArray.concat(secondArray);
-  if (array.length > maxLength) {
-    return array.slice(0, maxLength);
+console.log('Задача 2. Розрахунок калорій');
+function calcAverageCalories(days) {
+  let sum = 0;
+  let quantity = 0;
+  for (const day of days) {
+    sum += day.calories;
+    quantity += 1;
+  }
+  if (sum / quantity) {
+    return sum / quantity;
   } else {
-    return array;
+    return 0;
   }
 }
 
-console.log(makeArray(['Mango', 'Poly'], ['Ajax', 'Chelsea'], 3)); // ["Mango", "Poly", "Ajax"]
-console.log(makeArray(['Mango', 'Poly', 'Houston'], ['Ajax', 'Chelsea'], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
-console.log(makeArray(['Mango'], ['Ajax', 'Chelsea', 'Poly', 'Houston'], 3)); // ["Mango", "Ajax", "Chelsea"]
-console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 2)); // ["Earth", "Jupiter"]
-console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
-console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus', 'Venus'], 0)); // []
+console.log(
+  calcAverageCalories([
+    { day: 'monday', calories: 3010 },
+    { day: 'tuesday', calories: 3200 },
+    { day: 'wednesday', calories: 3120 },
+    { day: 'thursday', calories: 2900 },
+    { day: 'friday', calories: 3450 },
+    { day: 'saturday', calories: 3280 },
+    { day: 'sunday', calories: 3300 },
+  ]),
+); // 3180
+
+console.log(
+  calcAverageCalories([
+    { day: 'monday', calories: 2040 },
+    { day: 'tuesday', calories: 2270 },
+    { day: 'wednesday', calories: 2420 },
+    { day: 'thursday', calories: 1900 },
+    { day: 'friday', calories: 2370 },
+    { day: 'saturday', calories: 2280 },
+    { day: 'sunday', calories: 2610 },
+  ]),
+); // 2270
+console.log(calcAverageCalories([])); // 0
+
 console.log('_____________________________________________');
